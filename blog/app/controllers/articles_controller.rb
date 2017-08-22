74a4e2911008e5eda9d3a94a9df361ad5c378fba
Maria Laura Tarabillo <mlta18@outlook.com>
2017-08-22 11:48:33 -0400

    Set coming along

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,16 +1,28 @@
 
 class ArticlesController < ApplicationController
-  def new
-  end
 
+
+  def new
+     @article = Article.new
+  end
+  def index
+      @articles = Article.all
+    end
+
+  def show
+      @article = Article.find(params[:id])
+    end
 
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
-  end
+   if @article.save
+     redirect_to @article
+   else
+     render 'new'
+   end
+ end
 
   private
     def article_params
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   # //TODO: probably need props
+  validates :title, presence: true,
+                     length: { minimum: 5 }
+
 end
